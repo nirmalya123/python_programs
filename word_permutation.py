9ed1c2326d0e9116ce431942e3c6950d40b76232
nirmalya123 <31580771+nirmalya123@users.noreply.github.com>
2018-05-27 23:47:38 +0530

    Initial submit for word_permutation.py

--- word_permutation.py
+++ word_permutation.py
@@ -1,7 +1,8 @@
 '''
-He found a magazine and wants to know if he can cut out whole words from it and use them to create an untraceable replica of his ransom note. The words in his note are case-sensitive and he must use whole words available in the magazine, meaning he cannot use substrings or concatenation to create the words he needs.
+A person found a magazine and wants to know if he can cut out whole words from it and use them to create an untraceable replica of his ransom note. The words in his note are case-sensitive and he must use whole words available in the magazine, meaning he cannot use substrings or concatenation to create the words he needs.
 
 Given the words in the magazine and the words in the ransom note, print Yes if he can replicate his ransom note exactly using whole words from the magazine; otherwise, print No.
+https://www.hackerrank.com/challenges/ctci-ransom-note/problem
 '''
 from collections import Counter 
 def ransom_note(magazine, ransom):
